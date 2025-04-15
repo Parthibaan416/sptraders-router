081463bb6554bb8f2eca21ab06b202a66c39e646
parthibaan416 <parthishankar416@outlook.com>
2025-04-15 15:17:36 +0530

    first commit

---
+++ src/main/java/com/sptraders/sptraders_router/SptradersRouterApplication.java
@@ -0,0 +1,13 @@
+package com.sptraders.sptraders_router;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SptradersRouterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SptradersRouterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sptraders/sptraders_router/SptradersRouterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sptraders.sptraders_router;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SptradersRouterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
